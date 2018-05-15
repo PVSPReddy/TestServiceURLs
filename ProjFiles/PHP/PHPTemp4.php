1cae1b0a46b96ec7a049930dbd2a8a67e6f40ff2
Sivaprasad <pvsivapr@gmail.com>
2018-05-15 20:50:35 +0530

    Committing changes

--- ProjFiles/PHP/PHPTemp4.php
+++ ProjFiles/PHP/PHPTemp4.php
@@ -46,8 +46,21 @@ $responseArray = array_merge
 );
 echo json_encode($responseArray);
 */
+//method this aalso works
+/*
 $curl_response = file_get_contents($url);
 echo $curl_response;
+*/
+
+//$url = 'https://script.google.com/macros/s/#####/exec';
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl,CURLOPT_FOLLOWLOCATION, true);
+$response = curl_exec($curl);
+echo $response;
+curl_close($curl);
 //exit;
 //echo json_encode($curl_response);
 }
